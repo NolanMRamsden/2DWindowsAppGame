35fae43b6cced4370ba17f9c6a67f9956377548e
Joshuabaker2 <joshuabaker2@Hotmail.com>
2015-02-04 14:30:53 -0800

    Added difficulty counter

--- FieldFighter/FieldFighter/Hittable/Castles/AICastle.cs
+++ FieldFighter/FieldFighter/Hittable/Castles/AICastle.cs
@@ -10,17 +10,38 @@ namespace FieldFighter.Hittable.Castles
     class AICastle : Castle
     {
         const int thinkCount = 100;
+        
+        const int difficulty = 5;
 
+        private int leftUpgradeDelay = 2000;
+        private int rangeDelay = 600;
         private int counter = 0;
+        
+
         public AICastle(FieldFighter.Hittable.CharacterEnums.EDirection facing, int Xco) : base(facing, Xco) { }
         public override void updateCharacters(Castle enemyCastle)
         {
             counter++;
+            rangeDelay-=difficulty;
+            if (leftUpgradeDelay > 0)
+                leftUpgradeDelay-=difficulty;
+
             if (counter > thinkCount)
             {
                 counter = 0;
                 if (Math.Abs(enemyCastle.groundFrontTarget.getFrontLocationX() - getFrontLocationX()) < 1000)
                     spawn(ECharacterType.MELEE);
+                if (rangeDelay < 0)
+                {
+                    spawn(ECharacterType.RANGED);
+                    rangeDelay = 600;
+                }
+                if(leftUpgradeDelay == 0)
+                {
+                    upgradeLeft();
+                    leftUpgradeDelay = -1;
+                }
+
             }
             base.updateCharacters(enemyCastle);
         }
