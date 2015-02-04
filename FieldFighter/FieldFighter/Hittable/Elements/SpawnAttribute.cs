ba3fc34bb4c5ec684f96c15c842399f418236b78
Nolan Ramsden <rammer2@shaw.ca>
2015-02-04 14:19:58 -0800

    Added a spawn attribute class in prep for AI

---
+++ FieldFighter/FieldFighter/Hittable/Elements/SpawnAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldFighter.Hittable.Elements
+{
+    class SpawnAttribute
+    {
+        public int meleeFactor = 0;
+        public int rangeFactor = 0;
+        public int aoeFactor = 0;
+
+        public void add(SpawnAttribute a)
+        {
+            meleeFactor += a.meleeFactor;
+            rangeFactor += a.rangeFactor;
+            aoeFactor += a.aoeFactor;
+        }
+
+        public void sub(SpawnAttribute a)
+        {
+            meleeFactor -= a.meleeFactor;
+            rangeFactor -= a.rangeFactor;
+            aoeFactor -= a.aoeFactor;
+        }
+    }
+
+}
