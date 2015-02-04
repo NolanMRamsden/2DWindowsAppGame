76568deb4d5de1bd0df8eae0b976ce368974debf
Nolan Ramsden <rammer2@shaw.ca>
2015-02-03 19:19:46 -0800

    implemented a shitty number drawer

--- FieldFighter/FieldFighter/Utilities/NumberDrawer.cs
+++ FieldFighter/FieldFighter/Utilities/NumberDrawer.cs
@@ -10,14 +10,34 @@ namespace FieldFighter.Utilities
 {
     public class NumberDrawer
     {
+        private static Texture2D[] nums = new Texture2D[]
+        {
+            AnimationLoader.pngToTexture("Numbers/0.png"),
+            AnimationLoader.pngToTexture("Numbers/1.png"),
+            AnimationLoader.pngToTexture("Numbers/2.png"),
+            AnimationLoader.pngToTexture("Numbers/3.png"),
+            AnimationLoader.pngToTexture("Numbers/4.png"),
+            AnimationLoader.pngToTexture("Numbers/5.png"),
+            AnimationLoader.pngToTexture("Numbers/6.png"),
+            AnimationLoader.pngToTexture("Numbers/7.png"),
+            AnimationLoader.pngToTexture("Numbers/8.png"),
+            AnimationLoader.pngToTexture("Numbers/9.png"),
+        };
+
         public static void drawNumber(SpriteBatch batch, int number, int x, int y)
         {
-            drawNumber(batch, number, x, y, 20);
+            drawNumber(batch, number, x, y, 30);
         }
 
         public static void drawNumber(SpriteBatch batch, int number, int x, int y, int size)
         {
-            batch.Draw(RectangleGenerator.filled(20, 20), new Rectangle(x, y, number.ToString().Length * 20, 20), Color.White);
+            Char[] num = number.ToString().ToCharArray();
+            int xPoint = x;
+            foreach (Char c in num)
+            {
+                batch.Draw(nums[(int)Char.GetNumericValue(c)],new Rectangle(xPoint,y,size,(int)(size*1.5)),Color.White);
+                xPoint += size + 2;
+            }
         }
     }
 }
